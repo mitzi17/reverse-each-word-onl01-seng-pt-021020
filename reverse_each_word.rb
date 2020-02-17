131ec412898ac1867884a7dc4b287f9a413ba190
Mitzi Estrada <m_estrada17@live.com>
2020-02-17 07:52:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,18 @@
-sentence1 = "Hello there, and how are you?"
+sentence1 = ["Hello there, and how are you?"]
 
 def reverse_each_word(sentence1)
   "Hello there, and how are you?".reverse.split(" ").reverse.join(" ")
 end
 
 
-sentence2 = "Hi again, just making sure it's reversed!"
+sentence2 = ["Hi again, just making sure it's reversed!"]
+
+#def reverse_each_word(sentence2)
+  #"Hi again, just making sure it's reversed!".reverse.split(" ").reverse.join(" ")
+#end
 
 def reverse_each_word(sentence2)
-  "Hi again, just making sure it's reversed!".reverse.split(" ").reverse.join(" ")
+  sentence2.split.collect {|word| word.reverse}.join(" ")
 end
 
 
