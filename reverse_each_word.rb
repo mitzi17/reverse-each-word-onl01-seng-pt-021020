0035c187d280aab0e39c71f38d26a34eade9cd08
Mitzi Estrada <m_estrada17@live.com>
2020-02-17 07:45:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,5 +6,13 @@ end
 
 reverse_each_word(sentence1)
 
+sentence2 = "Hello there, and how are you?"
+
+def reverse_each_word(sentence1)
+  "Hello there, and how are you?".reverse.split(" ").reverse.join(" ")
+end
+
+reverse_each_word(sentence1)
+
 
 
