f6ee41020a1563d2ff5ba4c1da3bdf051327103c
Mitzi Estrada <m_estrada17@live.com>
2020-02-17 07:46:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,10 +6,10 @@ end
 
 reverse_each_word(sentence1)
 
-sentence2 = "Hello there, and how are you?"
+sentence2 = "Hi again, just making sure it's reversed!"
 
-def reverse_each_word(sentence1)
-  "Hello there, and how are you?".reverse.split(" ").reverse.join(" ")
+def reverse_each_word(sentence2)
+  "Hi again, just making sure it's reversed!".reverse.split(" ").reverse.join(" ")
 end
 
 reverse_each_word(sentence1)
