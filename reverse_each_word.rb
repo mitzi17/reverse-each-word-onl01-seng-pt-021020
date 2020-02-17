f29c6f0b58dc945ad8cf5fba93311f7ace245acd
Mitzi Estrada <m_estrada17@live.com>
2020-02-17 07:13:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence1 = "Hello there, and how are you?"
 
-def reverse_each_word(sentence1)
-  sentence1.split.collect {|word| word.reverse}.join(" ")
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 end
 
