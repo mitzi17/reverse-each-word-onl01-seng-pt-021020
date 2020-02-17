43244b910cc6fe17b91c05bb82c2fb669b7d3121
Mitzi Estrada <m_estrada17@live.com>
2020-02-17 07:57:56 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+sentence1 = ["Hello there, and how are you?"]
+
+def reverse_each_word(sentence1)
+  sentence1.reverse.split(" ").reverse.join(" ")
+end
+
+
+sentence2 = ["Hi again, just making sure it's reversed!"]
+
+#def reverse_each_word(sentence2)
+  #"Hi again, just making sure it's reversed!".reverse.split(" ").reverse.join(" ")
+#end
+
+def reverse_each_word(sentence2)
+  sentence2.split.collect {|word| word.reverse}.join(" ")
+end
+
+
+
+
